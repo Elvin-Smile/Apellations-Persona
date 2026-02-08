b5953ffa205d8e2ec19275b0ce9c0b80ed4393c3
Elvin-Smile <filip.ghostbuster@gmail.com>
2026-02-08 13:12:36 +0100

    Post option list

---
+++ 0z_Scenes/main_menu_buttons.gd
@@ -0,0 +1,18 @@
+extends OptionList
+
+func _process(delta):
+	move_pointer();
+	if (Input.is_action_just_pressed("Confirm")):
+		match (pointer):
+			0:
+				pass;
+			1:
+				pass;
+			2:
+				pass;
+			3:
+				pass;
+			4:
+				pass;
+			5:
+				pass;
---
+++ 0z_Scripts/option_list_class.gd
@@ -0,0 +1,28 @@
+@icon("res://option_list_icon.png")
+extends Control
+class_name OptionList
+
+
+var pointer : int = 0;
+var button_num : int;
+
+func _ready():
+	#hidea sve osim prvog pointera
+	button_num = get_child_count();
+	get_child(0).show();
+	for i in range(1, button_num):
+		get_child(i).hide();
+	print(button_num);
+
+func move_pointer() -> void:
+	#pointer selekcija
+	if ((Input.is_action_just_pressed("Down")) or (Input.is_action_just_pressed("Right"))):
+		get_child(pointer).hide();
+		pointer = (pointer+1)%button_num;
+		get_child(pointer).show();
+	if ((Input.is_action_just_pressed("Up")) or (Input.is_action_just_pressed("Left"))):
+		get_child(pointer).hide();
+		pointer -= 1;
+		if (pointer < 0):
+			pointer = button_num-1;
+		get_child(pointer).show();
