8a57b9ad83120eebaf97dcd46332931c63337b99
Elvin-Smile <filip.ghostbuster@gmail.com>
2026-02-22 18:02:58 +0100

    Skužio kak radi tile map

---
+++ Guy's/guys_street_tile_map_v00.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="guys_outside" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="street_tile_map_v00.png" width="192" height="192"/>
+</tileset>
