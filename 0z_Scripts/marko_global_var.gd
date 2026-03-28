869f5413d40f0db97afb52ac7ab27f466525b0b5
Elvin-Smile <filip.ghostbuster@gmail.com>
2026-03-28 19:49:11 +0100

    hepl

--- 0z_Scripts/marko_global_var.gd
+++ 0z_Scripts/marko_global_var.gd
@@ -1,5 +1,5 @@
 extends Node
 
-var coordinate : Vector2 = Vector2.ZERO;
+var coordinate : Vector2 = Vector2(48+8 , 160+8);
 var current_animation = 1;
-var goal : Vector2 = Vector2.ZERO;
+var goal : Vector2 = coordinate;
